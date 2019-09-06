43cf9e1f6ec2d73f81d942a4b4ac98598f81beb5
David Kerkeslager <kerkeslager@gmail.com>
2019-09-06 01:09:43 -0400

    Render text using an editor, which allows selection and copying

--- rendering.rkt
+++ rendering.rkt
@@ -15,24 +15,15 @@
                       [label "No events so far..."]))
 
     (define (render-text document)
-              (let* ([text (hash-ref document 'text)]
-                     [lines (string-split text "\n")]
-                     [canvas (new canvas%
-                                  [parent this]
-                                  [style (list 'vscroll)]
-                                  [paint-callback
-                                   (lambda (canvas dc)
-                                     (block
-                                      (send dc set-font (make-object font% 14 'modern))
-                                      (for ([i (in-naturals 0)]
-                                            [line lines])
-                                        (send dc draw-text line 5 (* 20 i)))))])])
-                (block (send canvas init-auto-scrollbars
-                             #f
-                             (+ 20 (* 20 (length lines)))
-                             0.0
-                             0.0)
-                       canvas)))
+      (let* ([text-string (hash-ref document 'text)]
+             [canvas (new editor-canvas% [parent this])]
+             [text (new text%)])
+        (block (send text change-style (make-object style-delta% 'change-size 14))
+               (send text change-style (make-object style-delta% 'change-family 'modern))
+               (send text insert text-string)
+               (send text lock #t)
+               (send canvas set-editor text)
+               canvas)))
 
     (define (render-error document)
       (new message%
